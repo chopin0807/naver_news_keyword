4f9aa3d7c4f2a49fdd87ff659b5fbf07a3883c24
unknown <chopin0807@naver.com>
2023-09-22 14:21:30 +0900

    크롤링 테스트를 통한 코드 정리 후 마무리

--- tesla_blog.py
+++ tesla_blog.py
@@ -45,10 +45,13 @@ def get_blog(url):
     # content_add(soup_blog, "span.se-fs-fs24", content_list)
     # print(re.findall("se[-_]fs.*", str(soup_blog.select_one("span"))))
     for i in soup_blog.select("span"): # 태그 패턴이 "se-fsxxxxx또는 se_fsxxxxx패턴을 모두 찾아 content_add매소드 적용"
-        if not len(re.findall("(?<=class=\")se[-_]fs.*", str(i))) == 0:
-            print(re.findall("(?<=class=\")se[-_]fs.*", str(i))[0].split(" ")[0])
-            content_add(soup_blog, "span." + re.findall("(?<=class=\")se[-_]fs.*", str(i))[0].split(" ")[0], content_list)
-    # print(soup_blog.select("span"))
+        find_pattern = re.findall("(?<=class=\")se[-_]fs.*", str(i))
+        pattern_only = []   # 리스트 find_pattern에서의 중복 제거 후 리스트
+        for value in find_pattern:
+            if value not in pattern_only:
+                pattern_only.append(value)
+        if not len(pattern_only) == 0:
+            content_add(soup_blog, "span." + pattern_only[0].split(" ")[0], content_list)
 
     content_result = ""
     for i in content_list:
